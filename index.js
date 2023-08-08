e6ca267441934ba47a433582d83d5099ed64c470
cliff-price <127143083+cliff-price@users.noreply.github.com>
2023-08-08 13:34:22 -0500

    Added myFlix API endpoints and updated Documentation
    Added myFlix API endpoints and updated Documentation

--- index.js
+++ index.js
@@ -12,34 +12,54 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {f
 
 let topTenMovies = [
     {
-      title: 'Movie 1',
+      title: 'Movie1',
+      genre: 'Genre1',
+      director: 'Director1'
     },
     {
-      title: 'Movie 2',
+      title: 'Movie2',
+      genre: 'Genre2',
+      director: 'Director2'
     },
     {
-      title: 'Movie 3',
+      title: 'Movie3',
+      genre: 'Genrea3',
+      director: 'Director3'
     },
     {
-      title: 'Movie 4',
+      title: 'Movie4',
+      genre: 'Genre4',
+      director: 'Director4'
     },
     {
-      title: 'Movie 5',
+      title: 'Movie5',
+      genre: 'Genre5',
+      director: 'Director5'
     },
     {
-      title: 'Movie 6',
+      title: 'Movie6',
+      genre: 'Genre6',
+      director: 'Director6'
     },
     {
-      title: 'Movie 7',
+      title: 'Movie7',
+      genre: 'Genre7',
+      director: 'Director7'
+         },
+    {
+      title: 'Movie8',
+      genre: 'Genre8',
+      director: 'Director8'
     },
     {
-      title: 'Movie 8',
+      title: 'Movie9',
+      genre: 'Drama9',
+      director: 'Director9'
     },
     {
-      title: 'Movie 9',
-    },
-    {
-      title: 'Movie 10',
+      title: 'Movie10',
+      genre: 'Genre10',
+      director: 'Director10'
     }
   ];
 
@@ -49,7 +69,7 @@ app.use(morgan('combined', {stream: accessLogStream}));
 // GET requests
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my movie page!  Enter "/movies" to see a list of my top 10 movies.  Enter "/documentation.html" for more help.');
+    res.send('Welcome to myFlix movie page!  Enter "/documentation.html" for more help.');
   });
 
   app.use(express.static('public'));
@@ -58,8 +78,51 @@ app.get('/', (req, res) => {
     res.json(topTenMovies);
   });
 
-  app.get('/secreturl', (req, res) => {
-    res.send('This is a secret url with supzer top-secret content.');
+/* 
+  Return a list of ALL movies to the user
+  app.get('/movies', (req, res) => {
+    res.send('Successful GET request returning data on all the movies');
+  });
+  */
+
+  //Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by movie's title to the user
+  app.get('/movies/:movieTitle', (req, res) => {
+    res.send('Successful GET request returning JSON data on a specific movie');
+  });
+
+  //Return data about a specific genre by genre's name to the user
+  app.get('/movies/genre/:genreName', (req, res) => {
+    res.send('Successful GET request returning a description of a specific genre');
+  });
+
+  //Return data about a director (bio, birth year, death year) by director's name
+  app.get('/movies/directors/:directorName', (req, res) => {
+    res.send('Successful GET request returning data on the director of a specific movie');
+  });
+
+  //Allow new users to register
+  app.post('/users', (req, res) => {
+    res.send('Successful GET request returning data showing the registration information and ID for a new user');
+  });
+
+  //Allow users to update their user info (username)
+  app.put('/users/:userName', (req, res) => {
+    res.send('Successful PUT request returning updated registration information for a specific user');
+  });
+
+  //Allow users to add a movie to their list of favorites (showing only a text that a movie has been added)
+  app.post('/users/movies/:movieName', (req, res) => {
+    res.send('Successful POST request showing that an "unspecified" movie has been added');
+  });
+
+  //Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed)
+  app.delete('/users/movies/:movieName', (req, res) => {
+    res.send('Successful DELETE request showing that an "unspecified" movie has been deleted');
+  });
+
+  //Allow existing users to deregister (showing only a text that a user email has been removed)
+  app.delete('/users/:userName', (req, res) => {
+    res.send('Successful DELETE request showing that an "unspecified" user has been deleted');
   });
 
 app.use(bodyParser.urlencoded({
